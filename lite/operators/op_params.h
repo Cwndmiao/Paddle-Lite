8bb63f56e4062a6d69fbd7caa0e3877a8d4efd31
miaotianxiang <miaotianxiang@baidu.com>
2020-06-22 20:00:06 +0800

    [LITE][XPU] accomodate resnet_cbam

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -338,6 +338,13 @@ struct ConcatParam : ParamBase {
   }
 };
 
+struct ClipParam : ParamBase {
+  lite::Tensor* X{};
+  lite::Tensor* Out{};
+  float min{0.0f};
+  float max{0.0f};
+};
+
 /// ----------------------- activation operators ----------------------
 struct ActivationParam : ParamBase {
   const lite::Tensor* X{};
@@ -1543,6 +1550,16 @@ struct XPUFcParam : ParamBase {
   float w_max{0.0f};
   bool transpose_w{true};
   std::string activation_type{""};
+};
+
+struct XPUResNetCbamParam : ParamBase {
+  lite::Tensor* input{};
+  std::vector<lite::Tensor*> filter;
+  std::vector<lite::Tensor*> bias;
+  std::vector<lite::Tensor*> max_filter;
+  lite::Tensor* output{};
+
+  float pool_p{1.0f};
 };
 
 // For DeformableConvolution op
